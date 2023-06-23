181b86f84f8cec10954be11405f84dc528081812
Joren Husic <husicjd@gmail.com>
2023-06-23 12:34:07 -0500

    refactored img-crop so that it can be used as a module

--- img-crop.py
+++ img-crop.py
@@ -1,3 +1,4 @@
+
 import tifffile
 import os
 import pathlib
@@ -14,6 +15,34 @@ STACK_CENTER_INDEX = (STACK_DEPTH - 1) // 2
 ##### FUNCTIONS
 #####
 
+def crop_images(sourceDirectory, targetDirectory, actuatorList):
+    for actuator in actuatorList:
+        calibrationImageSets = list(actuator.glob('*.tif*'))
+        actuatorPathName = actuator.parts[-1]
+        actuatorTargetDirectory = os.path.join(str(targetDirectory), actuatorPathName)
+        if not os.path.isdir(actuatorTargetDirectory):
+            os.mkdir(actuatorTargetDirectory)
+
+        for file in calibrationImageSets:
+            im = tifffile.imread(str(file))
+
+            stackCenter = im[STACK_CENTER_INDEX, :, :]
+            shape = stackCenter.shape
+            centerPixelX, centerPixelY = find_brightest_region(stackCenter)
+            centerPixelX, centerPixelY = pad_edge(shape, centerPixelX, centerPixelY)
+            print(f"Using center pixel ({centerPixelX}, {centerPixelY}) for cropping image at {file}.")
+
+            xmin = centerPixelX - RADIUS + 1
+            xmax = centerPixelX + RADIUS
+            ymin = centerPixelY - RADIUS + 1
+            ymax = centerPixelY + RADIUS
+
+            out = im[ : , xmin : xmax + 1 , ymin : ymax + 1]
+
+            writePath = os.path.join(actuatorTargetDirectory, "cropped_" + file.parts[-1])
+            tifffile.imwrite(writePath, out)
+
+
 def find_brightest_region(image):
     blurredImage = scipy.ndimage.gaussian_filter(image, sigma=7) # blurred to find brightest region over noise
     X, Y = np.where(blurredImage == blurredImage.max())
@@ -23,45 +52,43 @@ def find_int_mean(array):
     mean = np.sum(array) / array.size
     return int(mean)
 
-#####
-##### SETUP
-#####
+def pad_edge(shape, x, y):
+    retX = x
+    retY = y
 
-try:
-    sourceDirectory = pathlib.Path(sys.argv[1])
-    targetDirectory = pathlib.Path(sys.argv[2])
-    if not os.path.isdir(targetDirectory):
-        os.mkdir(targetDirectory)
-except IndexError:
-    print("usage: \"python img-crop.py {source_dir} {target_dir}\"")
-    exit(1)
+    if (x - RADIUS + 1) < 0:
+        retX = RADIUS - 1
+    if (y - RADIUS + 1) < 0:
+        retY = RADIUS - 1
+    if (x + RADIUS) > shape[0]:
+        retX = shape[0] - RADIUS
+    if (y + RADIUS) > shape[1]:
+        retY = shape[1] - RADIUS
 
-actuatorList = [x for x in sourceDirectory.iterdir() if x.is_dir()]
+    return retX, retY
+
+def setup():
+    try:
+        global sourceDirectory
+        sourceDirectory = pathlib.Path(sys.argv[1])
+        global targetDirectory
+        targetDirectory = pathlib.Path(sys.argv[2])
+        if not os.path.isdir(targetDirectory):
+            os.mkdir(targetDirectory)
+    except IndexError:
+        print("usage: \"python img-crop.py {source_dir} {target_dir}\"")
+        exit(1)
+
+    global actuatorList
+    actuatorList = [x for x in sourceDirectory.iterdir() if x.is_dir()]
 
 #####
 ##### CROP IMAGES
 #####
-    
-for actuator in actuatorList:
-    calibrationImageSets = list(actuator.glob('*.tif*'))
-    actuatorPathName = actuator.parts[-1]
-    actuatorTargetDirectory = os.path.join(str(targetDirectory), actuatorPathName)
-    if not os.path.isdir(actuatorTargetDirectory):
-        os.mkdir(actuatorTargetDirectory)
 
-    for file in calibrationImageSets:
-        im = tifffile.imread(str(file))
+if __name__ == "__main__":
+    setup()
 
-        stackCenter = im[STACK_CENTER_INDEX, :, :]
-        centerPixelX, centerPixelY = find_brightest_region(stackCenter)
-        print(f"Using center pixel ({centerPixelX}, {centerPixelY}) for cropping image at {file}.")
-
-        xmin = centerPixelX - RADIUS + 1
-        xmax = centerPixelX + RADIUS
-        ymin = centerPixelY - RADIUS + 1
-        ymax = centerPixelY + RADIUS
-
-        out = im[ : , xmin : xmax + 1 , ymin : ymax + 1]
-
-        writePath = os.path.join(actuatorTargetDirectory, "cropped_" + file.parts[-1])
-        tifffile.imwrite(writePath, out)
+    crop_images(sourceDirectory = sourceDirectory,
+                targetDirectory = targetDirectory,
+                actuatorList = actuatorList)
