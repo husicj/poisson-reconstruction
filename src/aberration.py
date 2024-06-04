fixed pupil function calculation

src/aberration.py
@@ -64,8 +64,10 @@ class Aberration:
             return self.gpf_
         grid = np.mgrid[0:self.size, 0:self.size] # an array of coordinates 
         uv_grid = self._pixel_to_pupil_coordinate(grid, microscope)
-        array = self.aberration_function(uv_grid[0], uv_grid[1])
-        gpf = MicroscopeImage(array, self.ffts, microscope, None)
+        array = np.exp(1j * self.aberration_function(uv_grid[0], uv_grid[1]))
+        r_grid = np.sqrt((uv_grid ** 2).sum(axis=0))
+        pupil = (r_grid <= microscope.numerical_aperture / microscope.wavelength.value)
+        gpf = MicroscopeImage(pupil * array, self.ffts, microscope, None)
         gpf.fourier_space = True
         self.gpf_ = gpf
         return gpf
@@ -77,8 +79,8 @@ class Aberration:
         """Converts a pixel index value to a pupil plane coordinate,
         based on the relevant microscope parameters."""
 
-        scale_factor = microscope.frequency_scale_factor.value / self.size
-        return scale_factor * pixel_indices
+        scale_factor = 0.5 / (microscope.pixel_size.value * (self.size // 2))
+        return scale_factor * (pixel_indices - self.size // 2)
 
     def psf(self,
             microscope: MicroscopeParameters

src/image.py
@@ -85,11 +85,16 @@ class DataImage(np.ndarray):
     def save(self, path):
         pass
 
-    def show(self, colorbar=False):
+    def show(self):
         """Plots the image represented by the class."""
-        ax = plt.imshow(self, cmap='gray')
-        if colorbar:
-            ax.colorbar()
+        if np.iscomplexobj:
+            fig, (ax0, ax1) = plt.subplots(1,2)
+            ax0.imshow(self.real, cmap = 'gray')
+            ax1.imshow(self.imag, cmap = 'gray')
+            ax0.set_title('real component')
+            ax1.set_title('imaginary component')
+        else:
+            ax = plt.imshow(self, cmap='gray')
         plt.show()
 
     def __array_finalize__(self, obj):

src/reconstruction.py
@@ -134,7 +134,7 @@ class PoissonReconstruction:
         """Runs a single iteration of the phase reconstruction algorithm."""
 
         print(f"Iteration {self.iteration_count}")
-        print(f"{self.search_direction_vector=}")
+        # print(f"{self.search_direction_vector=}")
         cost = self._line_search()
         print(f"{self.diversity_set.ground_truth_aberration - self.aberration.coefficients=}")
         self._update_object_estimate_and_search_direction()
@@ -235,5 +235,5 @@ if __name__ == "__main__":
     print(f"{np.mean(diversity_set.images[diversity_set.center_index])=}")
     recon = PoissonReconstruction(diversity_set)
     diversity_set.show()
-    recon.run(max_iterations=15)
+    recon.run(max_iterations=5)
     recon.image.show()

src/tests/test_aberration.py
@@ -1,30 +1,29 @@
+import sys
 import unittest
 
+import astropy.units as u
 import numpy as np
+
+sys.path.append('..')
 
 from aberration import Aberration, ZernikeAberration
 from data_loader import MicroscopeParameters
 
+
 class TestAberrationMethods(unittest.TestCase):
     def setUp(self):
-        self.test_microscope0 = MicroscopeParameters()
+        self.test_microscope0 = MicroscopeParameters(wavelength=0.24 * u.um)
         self.test_aberration0 = Aberration(lambda x, y: 0 * x * y, 4)
         self.test_aberration1 = Aberration(lambda x, y: x * y, 4)
-
-    def test__pixel_to_pupil_coordinate(self):
-        INPUT = np.mgrid[0:4, 0:4]
-        EXPECTED_RESULT = 0.532 / (1.2 * 4 * 0.104) * INPUT
-        OUTPUT = self.test_aberration0._pixel_to_pupil_coordinate(INPUT, self.test_microscope0)
-        self.assertTrue(
-                np.array_equal(OUTPUT, EXPECTED_RESULT)
-                )
+        self.test_aberration2 = Aberration(lambda x, y: 0 * x * y + 1, 4)
 
     def test_gpf(self):
-        EXPECTED_RESULT0 = np.zeros((4,4))
+        EXPECTED_RESULT0 = [[0,0,1,0],
+                            [0,1,1,1],
+                            [1,1,1,1],
+                            [0,1,1,1]]
         OUTPUT0 = self.test_aberration0.gpf(self.test_microscope0)
-        self.assertTrue(
-                np.array_equal(OUTPUT0, EXPECTED_RESULT0)
-                )
+        self.assertTrue(np.array_equal(OUTPUT0, EXPECTED_RESULT0))
 
         pix = self.test_aberration1._pixel_to_pupil_coordinate(1, self.test_microscope0)
         ab_per_pix = pix * np.exp(np.pi * 1j)
@@ -33,9 +32,24 @@ class TestAberrationMethods(unittest.TestCase):
                             [ 0 * ab_per_pix,  0 * ab_per_pix,  0 * ab_per_pix,  0 * ab_per_pix],
                             [-2 * ab_per_pix, -1 * ab_per_pix,  0 * ab_per_pix,  1 * ab_per_pix]]
         OUTPUT1 = self.test_aberration1.gpf(self.test_microscope0)
-        self.assertTrue(
-                np.array_equal(OUTPUT0, EXPECTED_RESULT0)
-                )
+        OUTPUT1.show()
+        # self.assertTrue(np.array_equal(OUTPUT1, EXPECTED_RESULT1))
+
+        OUTPUT2 = self.test_aberration2.gpf(self.test_microscope0)
+
+    def test_psf(self):
+        EXPECTED_RESULT0 = np.zeros((4,4))
+        OUTPUT0 = self.test_aberration0.psf(self.test_microscope0)
+        self.assertTrue(np.array_equal(OUTPUT0, EXPECTED_RESULT0))
+
+        OUTPUT1 = self.test_aberration1.psf(self.test_microscope0)
+
+        EXPECTED_RESULT2 = [[1, 0, 0, 0],
+                            [0, 0, 0, 0],
+                            [0, 0, 0, 0],
+                            [0, 0, 0, 0]]
+        OUTPUT2 = self.test_aberration2.psf(self.test_microscope0)
+        self.assertTrue(np.array_equal(OUTPUT2, EXPECTED_RESULT2))
 
 
 class TestZernikeAberrationMethods(unittest.TestCase):